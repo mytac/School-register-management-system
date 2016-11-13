aaea5100e435ad709ca3e0a956622230000af752
Tactics Chang <my_tac@126.com>
2016-11-13 21:02:54 +0800

    后台：登陆和教师信息管理

---
+++ php/register.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:PUT,GET,POST');
+header('Access-Control-Max-Age:60');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+header('Content-Type:application/json;charset=utf-8');
+//ajax_in
+$json=$_GET["data"];
+$d=json_decode($json,true);
+$get_id=$d[0]['id'];
+$get_psd=$d[0]['psd'];
+//db
+function t($s){return json_encode($s);}//转换字符串
+function query($a,$b,$c){
+    $sql="SELECT $a FROM allmember WHERE $b=$c";
+    $conn = mysqli_connect('localhost:3306', 'root', 'root','xueji');
+    $res=mysqli_query($conn,$sql);
+    return $a!="*"? mysqli_fetch_row($res):mysqli_fetch_array($res);
+}
+
+$row=query("id","id",$get_id);
+if($row){
+$real_psd=query("psd","id",$get_id)[0];
+    if($get_psd==$real_psd){
+        echo t(query("type","id",$get_id));
+    }else{echo t("wrong_psd");}
+}
+else{echo t("no_existed_id");}
+
+?>
---
+++ php/teacher/teacherSelfInfo.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: myTac
+ * Date: 2016/11/13
+ * Time: 17:21
+ */
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:PUT,GET,POST');
+header('Access-Control-Max-Age:60');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+header('Content-Type:application/json;charset=utf-8');
+
+//ajax_in
+$d=json_decode($_GET["data"],true);
+$get_id=$d[0]['id'];
+//db
+function query($a){
+    $sql="SELECT * FROM teacherinfo WHERE id=$a";
+    $conn = mysqli_connect('localhost:3306', 'root', 'root','xueji');
+    $res=mysqli_query($conn,$sql);
+    return mysqli_fetch_array($res);
+}
+
+echo json_encode(query($get_id));
+
+?>
